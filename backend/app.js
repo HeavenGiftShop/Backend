950f95536cc05ad9500c326a7948d4a06d96b3f4
unknown <samaata2016@gmail.com>
2023-12-08 20:57:31 +0200

    1st

---
+++ backend/app.js
@@ -0,0 +1,5 @@
+console.log('ay betengan')
+let books = [{"isbn": "1", "title": "Eloquent JavaScript, Second Edition"},
+{"isbn": "2", "title": "Learning JavaScript Design Patterns "},
+{"isbn": "3", "title": "Speaking JavaScript"}]
+console.log(books);
